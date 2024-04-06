96d619fe2247f8c0970059eba1cd691fc7f1fc29
ing.mateo <jhoan.franco@detektor.com.co>
2024-04-06 08:59:00 -0500

    add: endpoint find by id

--- ms-user/src/main/java/com/ms/user/controller/v2/doc/UserDoc.java
+++ ms-user/src/main/java/com/ms/user/controller/v2/doc/UserDoc.java
@@ -9,17 +9,15 @@ import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
-@Tag(name = "User" , description = "API exposed for management all user")
+@Tag(name = "User", description = "API exposed for management all user")
 @RequestMapping("/api/v2/user")
 public interface UserDoc {
 
 
     @Operation(summary = "create user"
-    ,description = "This operation is for creating user")
+            , description = "This operation is for creating user")
     @ApiResponses(value = {
             @ApiResponse(
                     responseCode = "201",
@@ -39,4 +37,12 @@ public interface UserDoc {
     })
     @PostMapping
     ResponseEntity<UserEntity> create(@RequestBody UserDto userDto);
+
+    @Operation(summary = "find by id user",
+            description = "This operation is for finding a user by ID. If the user with the specified ID is found, it will be returned. ID must be provided in the path parameter.")
+    @ApiResponse(responseCode = "200",
+            description = "The user with the specified ID was found",
+            content = @Content(mediaType = MediaType.APPLICATION_JSON_VALUE))
+    @GetMapping("/{id}")
+    ResponseEntity<UserEntity> getById(@PathVariable String id);
 }
