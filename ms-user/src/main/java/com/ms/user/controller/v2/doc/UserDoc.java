c7dac9c1e93a2cccf3e83998fae5480af27bda59
daniel florez <daniel0223@hotmail.es>
2024-04-06 09:30:28 -0500

    addding validations for DTO

--- ms-user/src/main/java/com/ms/user/controller/v2/doc/UserDoc.java
+++ ms-user/src/main/java/com/ms/user/controller/v2/doc/UserDoc.java
@@ -10,11 +10,7 @@ import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PatchMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 @Tag(name = "User" , description = "API exposed for management all user")
 @RequestMapping("/api/v2/user")
